07ff39499c5ec8b2f2fae9d4ee618b21a60cdfed
Austin Smith <trunk9slamchest@gmail.com>
2019-09-30 02:31:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,9 +40,9 @@ class Student
 		@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 	end
 
-	def self.create(name, grade)
-		student = Student.new(student)
-		# student.save
+	def self.create(student)
+		student = Student.new(student.values[0], student.values[1])
+		student.save
 		student
 	end
 
