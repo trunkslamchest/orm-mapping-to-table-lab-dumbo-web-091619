fdd5c96ef258c0fdb0a2106bdd5c952dfe1112a3
Austin Smith <trunk9slamchest@gmail.com>
2019-09-30 01:36:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,66 @@
 class Student
 
+class Student
+
+	attr_reader :id
+	attr_accessor :name, :grade
+
+	@@all = []
+
+	def initialize(id = nil, name, grade)
+		@id = id
+		@name = name
+		@grade = grade
+
+		@@all << self
+	end
+
+	def self.create_table
+		sql = <<-SQL
+				CREATE TABLE students (
+				id INTEGER PRIMARY KEY,
+				name TEXT,
+				grade INTEGER
+			);
+			SQL
+
+	DB[:conn].execute(sql)
+	end
+
+	def self.drop_table
+			sql = <<-SQL
+				DROP TABLE students;
+			);
+			SQL
+		DB[:conn].execute(sql)
+	end
+
+	def self.save
+		sql = <<-SQL
+		  INSERT INTO students (id, name, grade)
+		  VALUES (?, ?, ?)
+		SQL
+
+		DB[:conn].execute(sql, self.id, self.name, self.grade)
+	# database_connection.execute("INSERT INTO student (name, grade, id) VALUES (?, ?, ?)",name , grade, id)
+	end
+
+	def self.create(id, name, grade)
+		student = Cat.new(id, name, grade
+		student.save
+		student
+	end
+
+	# def self.all
+	# 	@@all
+	# end
+
+  # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
+
+end
+
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
