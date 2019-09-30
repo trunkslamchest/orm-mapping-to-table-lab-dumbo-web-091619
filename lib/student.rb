d694bda71105ee0cda96bd8193534c6264ea4a9b
Austin Smith <trunk9slamchest@gmail.com>
2019-09-30 01:41:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
 		  VALUES (?, ?, ?)
 		SQL
 
-		DB[:conn].execute(sql, self.name, self.grade)
+		DB[:conn].execute(sql, self.id, self.name, self.grade)
 	end
 
 	def self.create(name, grade)
