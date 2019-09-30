6b67359805a2204b36b6f849211e477104659276
Austin Smith <trunk9slamchest@gmail.com>
2019-09-30 01:59:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ class Student
 	end
 
 	def self.create(name, grade)
-		student = Student.new({name => grade})
+		student = Student.new(student.name, student.grade)
 		student.save
 		student
 	end
